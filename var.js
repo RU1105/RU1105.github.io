5e03f099650a64345394b41a4640098170454b81
kolp2514 <kolp2514@gmail.com>
2023-05-17 03:42:28 +0800

    six

---
+++ var.js
@@ -0,0 +1,23 @@
+var reservationCount = 87; // 初始化預約人數
+var nowreservationCount = 10;
+
+function increaseCount() {
+	reservationCount++; // 增加計數器
+    console.log(reservationCount); // 在控制台中顯示計數器的值
+}
+setInterval(increaseCount, 2000);
+
+function increaseReservationCount() {
+	reservationCount++; // 增加預約人數
+	document.getElementById("reservation-count").textContent = reservationCount; // 更新預約人數的元素內容
+}
+
+function nowReservationCount() {
+	nowreservationCount++; // 增加預約人數
+	document.getElementById("nowreservation-count").textContent = nowreservationCount; // 更新目前預約人數的元素內容
+}
+setInterval(nowReservationCount, 4000);
+
+function showQRCode() {
+	document.getElementById('image-container').src = "images/pic02.jpg";
+}
